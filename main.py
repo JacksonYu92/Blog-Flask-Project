6d7c4127d7e10065eef017d316be0ba61f95022f
JacksonYu92 <64330362+JacksonYu92@users.noreply.github.com>
2021-05-25 06:36:28 -0500

    Add files via upload
    add a Post class, a different way to do it.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
 def get_blogs():
     response = requests.get("https://api.npoint.io/ed99320662742443cc5b")
     response.raise_for_status()
     blogs = response.json()
-    return blogs
+    post_objects = [Post(blog['id'], blog['title'], blog['subtitle'], blog['body']) for blog in blogs]
+    return post_objects
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     blogs = get_blogs()
-    print(blogs)
     return render_template("index.html", blogs=blogs)
 
-@app.route('/post/<id>')
+@app.route('/post/<int:id>')
 def post(id):
-    blog = get_blogs()[int(id)-1]
-    return render_template("post.html", blog=blog)
+    requested_post = None
+    blogs =get_blogs()
+    for blog in blogs:
+        if blog.id == id:
+            requested_post = blog
+    return render_template("post.html", blog=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
